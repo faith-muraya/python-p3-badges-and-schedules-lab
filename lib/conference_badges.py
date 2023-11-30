9fb48fceaffd75afefabad3915ffd84085e3bdb2
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2023-11-30 15:37:57 +0300

    Completed assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,23 @@
 def badge_maker(name):
-    return None
+    message = "Hello, my name is " + name + "."
+    return message
 
 def batch_badge_creator(names):
-    return None
-
+    badges = []
+    for name in names:
+        badge = badge_maker(name)
+        badges.append(badge)
+    return badges
 def assign_rooms(names):
-    return None
+    new_list = [f"Hello, {name}! You'll be assigned to room {names.index(name) + 1}!" for name in names]
+    return new_list
+
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+
+    room_assignments = assign_rooms(names)
+    for assignment in room_assignments:
+        print(assignment)
